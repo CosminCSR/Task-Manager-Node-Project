7981d98d5d259cee533dba02d4b0677e9ec80b38
Cosmin <cosminrosu.csr@gmail.com>
2025-04-11 11:09:24 +0300

    version 3

--- starter/app.js
+++ starter/app.js
@@ -2,24 +2,29 @@ const connectDB = require('./db/connect')
 const express = require('express')
 const app = express()
 const tasks = require('./routes/tasks')
+const notFound = require('./middleware/not-found')
+const errorHandlerMiddleware = require('./middleware/error-handler')
 require('dotenv').config()
 
+//middleware
+app.use(express.static('./public'))
 app.use(express.json())
 
-app.get('/hello', (req, res) => {
-  res.send('Task Manager App')
-})
-
+//routes
 app.use('/api/v1/tasks', tasks)
+app.use(notFound)
+app.use(errorHandlerMiddleware)
 
-const port = 4444
+//use the environment variable PORT if defined, otherwise use 4444
+const port = process.env.PORT || 4444
 
+//function to start the server, while waiting for the database to connect
 const start = async () => {
   try{
     await connectDB(process.env.MONGO_URI)
-    app.listen(port, console.log(`Port ${port} works!!`))
-  } catch (error) {
-    console.log(error)
+    app.listen(port, console.log(`Port ${port} works!!`)) // shows in console that the port is working
+  } catch (error) { 
+    console.log(error) //shows in console that the port might not be working
   }
 }
 
